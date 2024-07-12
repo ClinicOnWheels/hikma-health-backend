641b7ff85e9607b4e3a959de9349857ec4b4025f
ClinicOnWheels <165398197+ClinicOnWheels@users.noreply.github.com>
2024-07-12 13:15:42 +0000

    Update config.py

--- app/config.py
+++ app/config.py
@@ -26,13 +26,13 @@ if ENV in ("dev_local", "dev_docker", "stg"):
 
 if ENV == "prod":
     FLASK_DEBUG = False
-    PG_USER = os.environ["DB_USER"]
+    PG_USER = os.environ["hacepersdb_user"]
     # PG_USER = os.environ["DB_NAME"]
-    PG_PASSWORD = os.environ["DB_PASSWORD"]
-    PG_HOST = os.environ["DB_HOST"]
+    PG_PASSWORD = os.environ["DOCjIFtgmit10xikniCzkbnNmHa37K6Z"]
+    PG_HOST = os.environ["dpg-cq6jpmss1f4s73e4vg50-a"]
     # PG_HOST = 'localhost'
-    PG_DB = os.environ["DB_NAME"]
-    PHOTOS_STORAGE_BUCKET = os.environ["PHOTOS_STORAGE_BUCKET"]
-    EXPORTS_STORAGE_BUCKET = os.environ["EXPORTS_STORAGE_BUCKET"]
+    PG_DB = os.environ["hacepersdb"]
+    PHOTOS_STORAGE_BUCKET = os.environ[""]
+    EXPORTS_STORAGE_BUCKET = os.environ[""]
     LOCAL_PHOTO_STORAGE_DIR = "/tmp/hikma_photos"
-    DEFAULT_PROVIDER_ID_FOR_IMPORT = os.environ["DEFAULT_PROVIDER_ID"]
+    DEFAULT_PROVIDER_ID_FOR_IMPORT = os.environ[""]
